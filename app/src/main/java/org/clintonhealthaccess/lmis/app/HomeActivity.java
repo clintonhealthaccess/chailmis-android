8f6fd13330186a113ccb67e84762486d0df189c7
Job Rwebembera <jobasiimwe@gmail.com>
2014-07-02 11:28:50 +0300

    [MJ,JR] added roboguice version 3 alpha2

--- app/src/main/java/org/clintonhealthaccess/lmis/app/HomeActivity.java
+++ app/src/main/java/org/clintonhealthaccess/lmis/app/HomeActivity.java
@@ -1,12 +1,13 @@
 package org.clintonhealthaccess.lmis.app;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import roboguice.activity.RoboActionBarActivity;
 
-public class HomeActivity extends Activity {
+
+public class HomeActivity extends RoboActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,21 +18,15 @@ public class HomeActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
         return super.onOptionsItemSelected(item);
     }
 
