805d5fe73c5328f62ec55eaa4189b4537e323872
wanderanimrod <wanderanimrod@gmail.com>
2014-07-03 09:25:14 +0300

    Inlining variable

--- app/src/main/java/org/clintonhealthaccess/lmis/app/HomeActivity.java
+++ app/src/main/java/org/clintonhealthaccess/lmis/app/HomeActivity.java
@@ -14,6 +14,7 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GraphViewSeries;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import roboguice.activity.RoboActionBarActivity;
 import roboguice.inject.InjectView;
@@ -22,8 +23,8 @@ import roboguice.inject.InjectView;
 public class HomeActivity extends RoboActionBarActivity {
 
     public static final String DATE_FORMAT = "dd/MM/yyyy";
-
     private TextView textFacilityName;
+
 
     @InjectView(R.id.layoutGraph)
     private LinearLayout layout;
@@ -41,15 +42,10 @@ public class HomeActivity extends RoboActionBarActivity {
         setContentView(R.layout.activity_home);
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-
         getSupportActionBar().setCustomView(R.layout.action_bar);
-
         setFacilityName("Kabira Health Center");
-
         setupGraph();
-
         setupAlerts();
-
     }
 
     private void setFacilityName(String text) {
@@ -61,43 +57,35 @@ public class HomeActivity extends RoboActionBarActivity {
         String[] values = new String[]{"Low Stock for Coartem", "Low Stock for Panadol", "Low Stock for Hedex"};
 
         ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; ++i) {
-            list.add(values[i]);
-        }
+        Collections.addAll(list, values);
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, list);
         listViewAlerts.setAdapter(adapter);
 
-
         String[] notificationValues = new String[]{"You Have a new Allocation"};
 
         ArrayList<String> notificationList = new ArrayList<String>();
-        for (int j = 0; j < notificationValues.length; ++j) {
-            notificationList.add(notificationValues[j]);
-        }
+        Collections.addAll(notificationList, notificationValues);
         ArrayAdapter notificationAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, notificationList);
         listViewNotifications.setAdapter(notificationAdapter);
     }
 
     private void setupGraph() {
-        GraphView graphView = new BarGraphView(
-                this
-                , "Commodity Consumption"
-        );
+        GraphView graphView = new BarGraphView(this, "Commodity Consumption");
         GraphViewSeries exampleSeries = new GraphViewSeries(new GraphView.GraphViewData[]{
-                new GraphView.GraphViewData(1, 3.0d)
-                , new GraphView.GraphViewData(2, 12d)
-                , new GraphView.GraphViewData(3, 4d)
-                , new GraphView.GraphViewData(4, 10d)
-                , new GraphView.GraphViewData(5, 6d)
-
+                new GraphView.GraphViewData(1, 3.0d),
+                new GraphView.GraphViewData(2, 12d),
+                new GraphView.GraphViewData(3, 4d),
+                new GraphView.GraphViewData(4, 10d),
+                new GraphView.GraphViewData(5, 6d)
         });
         graphView.addSeries(exampleSeries);
 
         layout.addView(graphView);
+        textFacilityName = (TextView) getSupportActionBar().getCustomView().findViewById(R.id.textFacilityName);
+        textFacilityName.setText("Kabira Health Center");
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
