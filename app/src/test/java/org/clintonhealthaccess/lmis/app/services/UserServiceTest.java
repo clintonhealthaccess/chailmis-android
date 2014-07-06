0e5ec15694a6663a8924076845ff17c7881bdc9b
Jeff Xiong <gigix1980@gmail.com>
2014-07-06 13:28:35 +0300

    Jeff - add product flavor specific config files to externalise DHIS2 base url configuration

--- app/src/test/java/org/clintonhealthaccess/lmis/app/services/UserServiceTest.java
+++ app/src/test/java/org/clintonhealthaccess/lmis/app/services/UserServiceTest.java
@@ -2,20 +2,24 @@ package org.clintonhealthaccess.lmis.app.services;
 
 import com.google.inject.Inject;
 
+import org.apache.http.client.methods.HttpUriRequest;
 import org.clintonhealthaccess.lmis.app.models.User;
 import org.clintonhealthaccess.lmis.utils.RobolectricGradleTestRunner;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.net.URI;
 import java.sql.SQLException;
 
 import static org.clintonhealthaccess.lmis.utils.TestInjectionUtil.setUpInjection;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.robolectric.Robolectric.addPendingHttpResponse;
+import static org.robolectric.Robolectric.getSentHttpRequest;
 
 @RunWith(RobolectricGradleTestRunner.class)
 public class UserServiceTest {
@@ -34,8 +38,13 @@ public class UserServiceTest {
         addPendingHttpResponse(200, "OK");
 
         User newUser = userService.register("admin", "district");
+
         assertThat(newUser, not(nullValue()));
         assertThat(userService.userRegistered(), is(true));
+
+        URI requestedUri = ((HttpUriRequest) getSentHttpRequest(0)).getURI();
+        URI expectedUri = URI.create("http://104.131.225.22:8888/dhis2/api/users");
+        assertThat(requestedUri, equalTo(expectedUri));
     }
 
     @Test(expected = ServiceException.class)
