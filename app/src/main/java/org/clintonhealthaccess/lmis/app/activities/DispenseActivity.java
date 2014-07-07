b4f677a2b65780527a013e26b40a307932e11035
Job Rwebembera <jobasiimwe@gmail.com>
2014-07-07 11:15:51 +0300

    [Jeff&Job added display of categories as buttons on dispence page]

--- app/src/main/java/org/clintonhealthaccess/lmis/app/activities/DispenseActivity.java
+++ app/src/main/java/org/clintonhealthaccess/lmis/app/activities/DispenseActivity.java
@@ -1,8 +1,13 @@
 package org.clintonhealthaccess.lmis.app.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import org.clintonhealthaccess.lmis.app.R;
+import org.clintonhealthaccess.lmis.app.models.Category;
+
+import java.util.List;
 
 public class DispenseActivity extends BaseActivity {
 
@@ -10,6 +15,16 @@ public class DispenseActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dispense);
+
+        LinearLayout categoriesLayout = (LinearLayout) findViewById(R.id.layoutCategories);
+
+        List<Category> categoryList = Category.all();
+
+        for (Category category : categoryList) {
+            Button button = new Button(this);
+            button.setText(category.getName());
+            categoriesLayout.addView(button);
+        }
     }
 
 
--- app/src/test/java/org/clintonhealthaccess/lmis/app/activities/DispenseActivityTest.java
+++ app/src/test/java/org/clintonhealthaccess/lmis/app/activities/DispenseActivityTest.java
@@ -1,18 +1,23 @@
 package org.clintonhealthaccess.lmis.app.activities;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import org.clintonhealthaccess.lmis.app.R;
 import org.clintonhealthaccess.lmis.utils.RobolectricGradleTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
 import static org.robolectric.Robolectric.buildActivity;
 
 @RunWith(RobolectricGradleTestRunner.class)
 public class DispenseActivityTest {
-
-
     private DispenseActivity getDispenseActivity() {
         return buildActivity(DispenseActivity.class).create().get();
     }
@@ -21,5 +26,19 @@ public class DispenseActivityTest {
     public void testBuildActivity() throws Exception {
         DispenseActivity dispenseActivity = getDispenseActivity();
         assertThat(dispenseActivity, not(nullValue()));
+    }
+
+    @Test
+    public void testShouldDisplayAllCategoriesAsButtons() throws Exception {
+        DispenseActivity dispenseActivity = getDispenseActivity();
+
+        LinearLayout categoryLayout = (LinearLayout) dispenseActivity.findViewById(R.id.layoutCategories);
+        int buttonAmount = categoryLayout.getChildCount();
+        assertThat(buttonAmount, is(6));
+
+        for(int i=0; i < buttonAmount; i++) {
+            View childView = categoryLayout.getChildAt(i);
+            assertThat(childView, instanceOf(Button.class));
+        }
     }
 }
