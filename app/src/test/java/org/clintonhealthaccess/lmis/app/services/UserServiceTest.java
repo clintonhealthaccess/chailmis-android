2c73384b291971323a82230971ff0919cdb125b5
James Muranga <jmured@gmail.com>
2014-07-16 18:15:59 +0300

    James -- removing check for url in test

--- app/src/test/java/org/clintonhealthaccess/lmis/app/services/UserServiceTest.java
+++ app/src/test/java/org/clintonhealthaccess/lmis/app/services/UserServiceTest.java
@@ -3,7 +3,6 @@ package org.clintonhealthaccess.lmis.app.services;
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
 
-import org.apache.http.client.methods.HttpUriRequest;
 import org.clintonhealthaccess.lmis.app.LmisException;
 import org.clintonhealthaccess.lmis.app.models.User;
 import org.clintonhealthaccess.lmis.app.sync.SyncManager;
@@ -13,19 +12,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.net.URI;
 import java.sql.SQLException;
 
 import static com.j256.ormlite.android.apptools.OpenHelperManager.releaseHelper;
 import static org.clintonhealthaccess.lmis.utils.TestInjectionUtil.setUpInjection;
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.robolectric.Robolectric.addPendingHttpResponse;
-import static org.robolectric.Robolectric.getSentHttpRequest;
 
 @RunWith(RobolectricGradleTestRunner.class)
 public class UserServiceTest {
@@ -59,10 +55,6 @@ public class UserServiceTest {
 
         assertThat(newUser, not(nullValue()));
         assertThat(userService.userRegistered(), is(true));
-
-        URI requestedUri = ((HttpUriRequest) getSentHttpRequest(0)).getURI();
-        URI expectedUri = URI.create("http://104.131.225.22:8888/dhis2/api/users");
-        assertThat(requestedUri, equalTo(expectedUri));
     }
 
     @Test(expected = LmisException.class)
