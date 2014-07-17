e03aacb4b22e3580b68f311484072d0c498042da
wanderanimrod <wanderanimrod@gmail.com>
2014-07-17 11:45:45 +0300

    [nim, mj] #X update reasons script using the new dhis2 test instance credentials

--- scripts/update_reasons.sh
+++ scripts/update_reasons.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-curl "http://104.131.225.22:8888/dhis2/api/systemSettings/reasons_for_order" -d '{"order_reasons":["Emergency","Routine"],"unexpected_quantity_reasons":["High Demand","Losses","Expirations","Adjustments"]}' -H "Content-Type: text/plain" -u admin:district -v
+curl "http://lmisdev.dhis2.org/api/systemSettings/reasons_for_order" -d '{"order_reasons":["Emergency","Routine"],"unexpected_quantity_reasons":["High Demand","Losses","Expirations","Adjustments"]}' -H "Content-Type: text/plain" -u tw_test:Secret123 -v
