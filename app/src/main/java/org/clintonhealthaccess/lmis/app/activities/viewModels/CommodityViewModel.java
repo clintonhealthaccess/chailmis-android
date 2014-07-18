e14f2209364188a92f0d22a8394660e56641220d
Job Rwebembera <jobasiimwe@gmail.com>
2014-07-18 13:22:19 +0300

    Job, Tim adds order fields (startDate, endDate, reasonPosition) to the commodityViewModel

--- app/src/main/java/org/clintonhealthaccess/lmis/app/activities/viewModels/CommodityViewModel.java
+++ app/src/main/java/org/clintonhealthaccess/lmis/app/activities/viewModels/CommodityViewModel.java
@@ -1,14 +1,20 @@
 package org.clintonhealthaccess.lmis.app.activities.viewmodels;
 
 import org.clintonhealthaccess.lmis.app.models.Commodity;
+import org.clintonhealthaccess.lmis.app.models.OrderReason;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.Map;
 
 public class CommodityViewModel implements Serializable {
     private boolean selected;
     private int quantityEntered;
     private Commodity commodity;
     private int quantityPopulated;
+
+    private Integer orderReasonPosition;
+    private Date orderPeriodStartDate, orderPeriodEndDate;
 
     public CommodityViewModel(Commodity commodity) {
         this.commodity = commodity;
@@ -42,6 +48,10 @@ public class CommodityViewModel implements Serializable {
         return commodity.stockIsFinished();
     }
 
+    public int getOrderDuration(){
+        return commodity.getOrderDuration();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -67,5 +77,29 @@ public class CommodityViewModel implements Serializable {
 
     public void setQuantityPopulated(int quantityPopulated) {
         this.quantityPopulated = quantityPopulated;
+    }
+
+    public Date getOrderPeriodEndDate() {
+        return orderPeriodEndDate;
+    }
+
+    public void setOrderPeriodEndDate(Date orderPeriodEndDate) {
+        this.orderPeriodEndDate = orderPeriodEndDate;
+    }
+
+    public Date getOrderPeriodStartDate() {
+        return orderPeriodStartDate;
+    }
+
+    public void setOrderPeriodStartDate(Date orderPeriodStartDate) {
+        this.orderPeriodStartDate = orderPeriodStartDate;
+    }
+
+    public Integer getOrderReasonPosition() {
+        return orderReasonPosition;
+    }
+
+    public void setOrderReasonPosition(Integer orderReasonPosition) {
+        this.orderReasonPosition = orderReasonPosition;
     }
 }
