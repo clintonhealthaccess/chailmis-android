c3883a2cddc1bc53c86b22ee610e414e55c13024
Jeff Xiong <gigix1980@gmail.com>
2014-09-26 11:37:59 +0300

    more meaningful (I hope) log message

--- app/src/main/java/org/clintonhealthaccess/lmis/app/services/AllocationService.java
+++ app/src/main/java/org/clintonhealthaccess/lmis/app/services/AllocationService.java
@@ -190,7 +190,7 @@ public class AllocationService {
                 try {
                     allocationItem.setQuantity(parseInt(input.getValue()));
                 } catch (NumberFormatException e) {
-                    Log.i("Allocation Amount Not Integer", input.getCommodityAction().getActivityType() + " - " + input.getValue());
+                    Log.e("Allocation Amount Not Integer", input.getCommodityAction().getActivityType() + " - " + input.getValue());
                 }
                 allocationItem.setCommodity(input.getCommodityAction().getCommodity());
                 return allocationItem;
