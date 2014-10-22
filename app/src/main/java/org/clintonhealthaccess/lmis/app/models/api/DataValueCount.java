021af0021da3b8f92de68ae34aaaa2ceb60f91db
James Muranga <jmured@gmail.com>
2014-10-22 13:24:32 +0300

    [MJ] updated the copyright message

--- app/src/main/java/org/clintonhealthaccess/lmis/app/models/api/DataValueCount.java
+++ app/src/main/java/org/clintonhealthaccess/lmis/app/models/api/DataValueCount.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2014, ThoughtWorks
- *
+ * Copyright (c) 2014, Thoughtworks Inc
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
