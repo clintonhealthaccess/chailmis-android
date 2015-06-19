b1e088f53c7201fbc7a3da5ef1145efd2eb31074
Han Lin <hlin@CNhlin.local>
2015-06-19 13:42:22 +0800

    fix issue: when login faild , progress dialog didn't close

--- app/src/main/java/org/clintonhealthaccess/lmis/app/activities/RegisterActivity.java
+++ app/src/main/java/org/clintonhealthaccess/lmis/app/activities/RegisterActivity.java
@@ -173,7 +173,7 @@ public class RegisterActivity extends OrmLiteActivity implements Serializable {
 
         @Override
         protected void onPostExecute(Boolean succeeded) {
-            if (dialog.isShowing() && dialog.getOwnerActivity()!=null && !dialog.getOwnerActivity().isFinishing()) {
+            if (dialog.isShowing()) {
                 dialog.dismiss();
             }
             if (succeeded) {
