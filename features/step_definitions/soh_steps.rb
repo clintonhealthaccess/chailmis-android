27f31aea15021bec115465ede011992ed5351aaa
Itamar Hassin <ihassin@mac.com>
2015-10-05 16:39:53 -0400

    Added SOH feature based on emails with Egwuma

---
+++ features/step_definitions/soh_steps.rb
@@ -0,0 +1,35 @@
+#
+# Please do not implement yet, this is a demo; in "negotiations" with Egwuma still.
+#
+
+Given(/^the server has "(.*?)" items of type "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I update SOH with "(.*?)" "(.*?)" "(.*?)"$/) do |arg1, arg2, arg3|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I wait for the sync period at "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the sync succeeds$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the server shows that there are "(.*?)" items of type "(.*?)" for my clinic$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the sync fails$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I re\-install the app on the tablet$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the SOH shown on the tablet for item of type "(.*?)" is "(.*?)" for my clinic$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
